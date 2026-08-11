3dc17c6bc2feadf56b189c883404afee895dbf08
sqaredaqi70-dot <sqaredaqi70@gmail.com>
2026-08-11 07:08:18 +0330

    Add package init file

---
+++ modules/geomechanics_1d_mem/src/__init__.py
@@ -0,0 +1,15 @@
+"""
+1D Mechanical Earth Model Module
+================================
+
+Part of the Geomechanics Python Framework.
+
+Author:  Saeed Gharedaghi
+Contact: sqaredaqi70@gmail.com
+License: AGPL-3.0 / Commercial
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Sqared Aqi'
+__email__ = 'sqaredaqi70@gmail.com'
+__license__ = 'AGPL-3.0'
